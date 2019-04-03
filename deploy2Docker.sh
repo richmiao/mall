9ac002120c2e575991fe35dd11ddc6c8bf6b2b57
Richard Miao <richard.miao@pwc.com>
2019-04-04 05:54:17 +0800

    update

--- deploy2Docker.sh
+++ deploy2Docker.sh
@@ -1,13 +1,17 @@
 cd ./mall-mbg
 mvn clean package
+mvn install
 
 cd ../mall-search
+mvn install
 mvn clean package docker:build
 
 cd ../mall-admin
+mvn install
 mvn clean package docker:build
 
 cd ../mall-portal
+mvn install
 mvn clean package docker:build
 
 cd ../document/docker
